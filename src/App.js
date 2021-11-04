1f08b5e0d9ab28016dbf2fdb40f6a9c239e5136b
nchvrgr <nachotoasies@gmail.com>
2021-11-04 11:07:03 -0300

    attempt no. 2000

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import './index.css';
 
 
 const API_KEY = 'c18fb43629294471b30af9c552ed7c9e';
+const API_URL = 'https://newsapi.org';
 
 const App = () => {
   const [categoria, setCategoria] = useState('');
@@ -16,7 +17,7 @@ const App = () => {
 
   useEffect(() => {
     const consultarAPI = async () => {
-      const url = `/v2/top-headlines?country=ar&category=${categoria}&apiKey=${API_KEY}`;
+      const url = `${API_URL}/v2/top-headlines?country=ar&category=${categoria}&apiKey=${API_KEY}`;
       const respuesta = await axios.get(url);
       const {data} = await respuesta;
       console.log(data);
